f933dd86ea6b015948cb62f158c1cc34c93fc5cd
Aki78 <kmorooka78@gmail.com>
2022-09-09 11:44:52 +0300

    changed table from airports to airport

--- connect_py.py
+++ connect_py.py
@@ -1,6 +1,6 @@
 import mysql.connector
 def get_employees_byLast_name():
-    sql = "SELECT name from airports"
+    sql = "SELECT name from airport"
     # sql += " WHERE Last_name='" + last_name + "'"
     print(sql)
     cursor = connection.cursor()
@@ -11,21 +11,6 @@ def get_employees_byLast_name():
             # print(f"Hello! I'm {row[2]} {row[1]}. My salary is {row[3]} euros per month.")
             print(row)
     return
-choose = input("would you like to A: add, F:find or Q:Quit ")
-
-def add_airport(x):
-    print(x)
-
-def find_airport(x):
-    print(x)
-
-while 1:
-    if choose == 'A':
-        add_airport("A")
-    elif choose == 'F':
-        find_airport()
-    else:
-        break
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
