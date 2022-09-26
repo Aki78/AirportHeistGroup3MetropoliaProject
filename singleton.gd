b24977eb17b1a2ab521466f713789dc7bdd2c62c
MKRadius <caomanhkhai110@gmail.com>
2022-09-26 14:43:55 +0300

    Source

---
+++ singleton.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var background_color = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
