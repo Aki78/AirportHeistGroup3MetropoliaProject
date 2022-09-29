df6d32ca84be5b3c3bba1b23672b968d3f368fc7
Aki78 <kmorooka78@gmail.com>
2022-09-29 14:20:08 +0300

    added game core

---
+++ Python/game.py
@@ -0,0 +1,8 @@
+def theft_success_rate():
+    pass
+
+def get_selling_price():
+    pass
+
+def co2_amount():
+    pass
--- Python/http.py
+++ Python/http.py
@@ -1,11 +1,3 @@
 def deg_to_xy():
     pass
 
-def theft_success_rate():
-    pass
-
-def get_selling_price():
-    pass
-
-def co2_amount():
-    pass
