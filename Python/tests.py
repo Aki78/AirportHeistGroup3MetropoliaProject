4092c2a5ff8fc4202b073e85121c969ab7c2a3aa
Aki78 <kmorooka78@gmail.com>
2022-09-30 14:06:27 +0300

    more tests

--- Python/tests.py
+++ Python/tests.py
@@ -7,4 +7,5 @@ def test_meters_to_km():
     assert 1 == helper.meters_to_km(1000)
 def test_get_distances():
     assert  10000 < helper.get_distances((3.4, -75),(60.3, 24.9))
+    assert  11000 > helper.get_distances((3.4, -75),(60.3, 24.9))
 
