38b5c79b281a412f6cb95a973ec65b3cb21c5b0d
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-10-05 14:29:15 +0300

    unknown

--- Python/core2_frank.py
+++ Python/core2_frank.py
@@ -1,3 +1,6 @@
 print("Welcome to airport heist!")
 print("1. Play \n2. Settings \n3. Instructions \n4. Credits \n5. Exit")
 user_input = input("What do you want to do: ")
+
+if user_input == "" or user_input > 5 or user_input < 1:
+    print("unknown input")
