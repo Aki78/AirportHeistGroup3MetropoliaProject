e3eac3a9ab5fbf2482c9297519f13c18814b46d8
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-10-05 16:35:23 +0300

    unknown

--- Python/core2_frank.py
+++ Python/core2_frank.py
@@ -1,22 +1,27 @@
-
+import os
 def main_screen():
     print("Welcome to airport heist!")
     print("1. Play \n2. Settings \n3. Instructions \n4. Credits \n5. Exit")
+    user_input = str(input("What do you want to do: "))
+    return user_input
+
+def setting_screen():
+    print("Settings")
+    print("1. Easy \n2. Hard \n3. Return to main menu")
+    settings_input = str(input("What do you want to do: "))
+    return settings_input
 
 main_screen()
-user_input = input("What do you want to do: ")
-
-if user_input == "" or user_input > 5 or user_input < 1:
+os.system('cls')
+if main_screen() == "" or int(main_screen()) > 5 or int(main_screen()) < 1:
     print("unknown input")
     main_screen()
 
 #if user_input == "1":
 
-if user_input == "2":
-    print("Settings")
-    print("1. Easy \n2. Hard \n3. Return to main menu")
-    settings_input = input("What do you want to do: ")
-    if user_input == "" or user_input > 3 or user_input < 1:
+if main_screen() == 2:
+    setting_screen()
+    if setting_screen() == "" or int(setting_screen()) > 3 or int(setting_screen() < 1):
         print("unknown input")
-        main_screen()
+        setting_screen()
 
