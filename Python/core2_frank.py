6e14eca6768578381395936091a8bc5ed1c79153
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-10-05 22:47:08 +0300

    playing

--- Python/core2_frank.py
+++ Python/core2_frank.py
@@ -1,5 +1,7 @@
 import os
+import time
 def main_screen():
+    os.system('cls')
     while True:
         print("Welcome to airport heist!")
         print("1. Play \n2. Settings \n3. Instructions \n4. Credits \n5. Exit")
@@ -9,30 +11,36 @@ def main_screen():
         if user_input == "" or int(user_input) > 5 or int(user_input) < 1:
             print("unknown input")
             main_screen()
-        elif user_input == "2":
+        if user_input == "2":
             setting_screen()
-        elif user_input == "3":
+        if user_input == "3":
             instructions()
-        elif user_input == "4":
+        if user_input == "4":
             credits()
-
-        else:
-            return user_input
+        if user_input == "5":
+            break
 
 def setting_screen():
+    os.system('cls')
     while True:
         print("Settings")
-        print("1. Easy \n2. Hard \n3. Return to main menu")
+        print("Choose the game level\n1. Easy \n2. Hard")
         settings_input = str(input("What do you want to do: "))
 
         if settings_input == "" or int(settings_input) > 3 or int(settings_input) < 1:
             print("unknown input")
-        elif settings_input == "3":
+            setting_screen()
+        if settings_input == "1":
+            print("Good choice!")
+            time.sleep(1.5)
             main_screen()
-        else:
-            return settings_input
+        if settings_input == "2":
+            print("Good luck!")
+            time.sleep(1.5)
+            main_screen()
 
 def instructions():
+    os.system('cls')
     while True:
         print("Instructions")
         print("Text")
@@ -44,6 +52,7 @@ def instructions():
             main_screen()
 
 def credits():
+    os.system('cls')
     while True:
         print("Credits")
         print("Airport Heist was created by the most awesome of foursomes!\nAki Morooka \nKhai cao \nKiana Aghajani \nFrancesco Natanni\nAll work is our own, so all credit goes to us.")
