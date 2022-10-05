ed537d2b83329e227068333449e7af0623f92c32
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-10-05 20:42:05 +0300

    unknown

--- Python/core2_frank.py
+++ Python/core2_frank.py
@@ -1,28 +1,32 @@
 import os
 def main_screen():
-    print("Welcome to airport heist!")
-    print("1. Play \n2. Settings \n3. Instructions \n4. Credits \n5. Exit")
-    user_input = str(input("What do you want to do: "))
+    while True:
+        print("Welcome to airport heist!")
+        print("1. Play \n2. Settings \n3. Instructions \n4. Credits \n5. Exit")
+        print()
+        user_input = str(input("What do you want to do: "))
 
-    if user_input == "" or int(user_input) > 5 or int(user_input) < 1:
-        print("unknown input")
-    return user_input
+        if user_input == "" or int(user_input) > 5 or int(user_input) < 1:
+            print("unknown input")
+
+        else:
+            return user_input
 
 def setting_screen():
     print("Settings")
     print("1. Easy \n2. Hard \n3. Return to main menu")
     settings_input = str(input("What do you want to do: "))
-    return settings_input
 
-while True:
-    main_screen()
-    print(main_screen())
+    if settings_input == "" or int(settings_input) > 3 or int(settings_input) < 1:
+        print("unknown input")
+    else:
+        return settings_input
 
-    #if main_screen() == "1":
 
-    if main_screen() == 2:
-        setting_screen()
-        if setting_screen() == "" or int(setting_screen()) > 3 or int(setting_screen() < 1):
-            print("unknown input")
-            setting_screen()
+main_screen()
+if main_screen() == "2":
+    setting_screen()
+    if setting_screen() == "3":
+        main_screen()
+
 
