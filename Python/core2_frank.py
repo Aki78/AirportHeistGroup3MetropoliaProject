741280b144c92ba814bc98db1d8b85fa4bed97a4
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-10-05 21:01:18 +0300

    unknown

--- Python/core2_frank.py
+++ Python/core2_frank.py
@@ -8,25 +8,29 @@ def main_screen():
 
         if user_input == "" or int(user_input) > 5 or int(user_input) < 1:
             print("unknown input")
+            main_screen()
+        elif user_input == "2":
+            setting_screen()
 
         else:
             return user_input
 
 def setting_screen():
-    print("Settings")
-    print("1. Easy \n2. Hard \n3. Return to main menu")
-    settings_input = str(input("What do you want to do: "))
+    while True:
+        print("Settings")
+        print("1. Easy \n2. Hard \n3. Return to main menu")
+        settings_input = str(input("What do you want to do: "))
 
-    if settings_input == "" or int(settings_input) > 3 or int(settings_input) < 1:
-        print("unknown input")
-    else:
-        return settings_input
+        if settings_input == "" or int(settings_input) > 3 or int(settings_input) < 1:
+            print("unknown input")
+        elif settings_input == "3":
+            main_screen()
+        else:
+            return settings_input
 
 
 main_screen()
-if main_screen() == "2":
-    setting_screen()
-    if setting_screen() == "3":
-        main_screen()
+
+
 
 
