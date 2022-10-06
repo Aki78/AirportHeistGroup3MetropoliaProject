149a04a3955096a371ecc0ddfc55cffdf077dfc2
kiianaaa <kiana.aghajani@metropolia.fi>
2022-10-06 18:00:07 +0300

    "The mode"

--- Python/core.py
+++ Python/core.py
@@ -40,9 +40,15 @@ def print_settings():
 def print_instructions():
     os.system("cls")
     print("This is the instructions")
-    print("1 - ")
-    print("2 - ")
-    print("3 - ")
+    print("1 - You are limited by budget and stamina.")
+    print("2 - You have to by tickets to travel.")
+    print("3 - In order to acquire money, you have to steal.")
+    print("4 - You can get caught by the interpol, but you are informed about the odds.")
+    print("5 - You can see in which possible cities the interpol will be, but not where they will go next.")
+    print("6 - You are restricted to airports with-in a certain distance.")
+    print("7 - You will lose if you go under stamina or budget.")
+
+
     input("Press Enter to continue...")
     return
 
