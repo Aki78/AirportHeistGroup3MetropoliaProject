d06c15cf2f1ee37b9b754a7d7fe6ddde5c63f0f7
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-10-08 15:06:13 +0300

    minor text edit

--- Python/settings.py
+++ Python/settings.py
@@ -4,13 +4,13 @@ def mode():
     print("2- Hard")
     
     play_mode = input("Your selection: ")
-    if play_mode == "1":
+    if play_mode == "1" or play_mode == 'Easy':
         stamina = 1000
         budget = 10000
         rate_up = 1.0
         rate_down = 0
         travel_distance = 1000
-    elif play_mode == "2":
+    elif play_mode == "2" or play_mode == 'Hard':
         stamina = 700
         budget = 5000
         rate_up = 0.5
