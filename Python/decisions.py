f947df155ba0187be40dae51d592d1df32bb4f2b
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-10-12 18:39:36 +0300

    readme

--- Python/decisions.py
+++ Python/decisions.py
@@ -11,7 +11,6 @@ def mode():
     print("2- Hard")
     
     play_mode = input("Your selection: ")
-    # play_mode = "1" 
 
     if play_mode == "1" or play_mode == "Easy":
         return 5000, 10000, 1.0, 0, 1000
