5adbf66f1a30e963d9e99e846fa9b7a08a4d7185
Aki78 <kmorooka78@gmail.com>
2022-11-17 13:46:41 +0200

    pushgin react init

---
+++ react/src/App.js
@@ -0,0 +1,34 @@
+import spinner from './img/spina.gif'
+import background from './img/main_background.png'
+
+import './App.css';
+
+const Play = () => {
+
+    return (
+        <>
+            
+            <div id='folder-background'></div>
+        </>
+    )
+
+}
+
+const App = () => {
+    return (
+        
+        <>
+            <img src={background} alt='background'></img>
+            <div id='body-content'></div>
+                
+                
+            
+
+            
+        </>
+        
+    )
+}
+
+
+export default App;
