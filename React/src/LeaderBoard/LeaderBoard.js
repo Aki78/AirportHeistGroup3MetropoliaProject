cac72ffb08ba517141e1471d251485c7d3a1d02e
Aki78 <kmorooka78@gmail.com>
2022-11-17 16:29:05 +0200

    updated leaderboard css

---
+++ React/src/LeaderBoard/LeaderBoard.js
@@ -0,0 +1,46 @@
+import './leadbrd.css'
+import React from 'react'
+
+const LeaderBoard = () => {
+    return (
+        <>
+                <main class="main">
+                    <div class="leadbrd__h1">
+                        <h1>LeaderBoard</h1>
+                    </div>
+                    <div class="section">
+                        <div class="chart">
+                            <div class="leadbrd">
+                                <div class="top">
+                                    <h1>Top Ten Players</h1>
+                                </div>
+                                <div class="ppl">
+                                    <ol>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+        </>
+        
+    )
+}
+
+
+export default LeaderBoard;
+
+
+
+
+
