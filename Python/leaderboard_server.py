d1dd92464d21f2ea8a0151221dcaa164f4fe820a
Jenni <jenni.hallikas@metropolia.fi>
2022-11-28 12:28:34 +0200

    commit

---
+++ Python/leaderboard_server.py
@@ -0,0 +1,38 @@
+import mysql.connector
+import tabulate
+
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port= 3306,
+    database='eu_flight_game',
+    user='root',
+    password='root',
+    autocommit=True
+)
+
+def add_leaderboard_data(game_state):
+    sql = (f"insert into top_players VALUES(\"{game_state['player']}\",{game_state['budget']});")
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+
+player = "jenni2"
+budget = 900
+game_state = {
+    "player": player,
+    "budget": budget
+
+}
+add_leaderboard_data(game_state)
+
+def get_leaderboard_data():
+    sql = ("select * from top_players order by score desc limit 10;")
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print("\nTop 5 players:")
+    print(tabulate(result, headers=["Player", "Score"], tablefmt="double_outline"))
+
+
+
+
---
+++ Python/tests.py
@@ -0,0 +1,39 @@
+import pytest
+import helper
+import gfuncs
+
+
+def test_feet_to_meters():
+    assert 3048 == int(round(10000*helper.feet_to_meters(1)))
+
+def test_meters_to_km():
+    assert 1000 == int(helper.meters_to_km(1))
+
+def test_get_distances():
+    assert  10000 < helper.get_distances((3.4, -75),(60.3, 24.9))
+    assert  11000 > helper.get_distances((3.4, -75),(60.3, 24.9))
+
+def test_get_possible_flights():
+    result = helper.get_possible_flights((35.46, 15.06), [(49.62,6.20),(28.45,-13.86),(44.57,26.08),(53.52,-6.27),(39.52,-121.76),(29.10, -95.77)])
+    print(result)
+    assert result == [0, 1, 2, 3]
+
+def test_deg_to_xy():
+    assert 111  == round(helper.deg_to_xy((0,1))[1])
+    assert round(0.63*111)  == round(helper.deg_to_xy((1,0))[0])
+
+def test_get_min_max_distance():
+    print("testing started: ", helper.get_min_max_distance([(1,1), (2,2), (3,3), (4,4)]))
+    assert True
+
+def test_get_ticket_price():
+    distance = helper.get_distances((52.30, 4.76), (37.93, 23.94))
+    result = gfuncs.get_ticket_price(distance)
+    assert result == 3272
+
+
+
+
+
+
+
