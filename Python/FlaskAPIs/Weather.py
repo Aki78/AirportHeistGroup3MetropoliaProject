064d78b4978dbbd9341f965e96b9afcdff3dd3d6
kiianaaa <kiana.aghajani@metropolia.fi>
2022-12-01 00:02:55 +0200

    commit

--- Python/FlaskAPIs/Weather.py
+++ Python/FlaskAPIs/Weather.py
@@ -3,16 +3,4 @@ import requests
 from flask import Flask, Response, request
 
 
-layer = "clouds_new"
-z = 200
-x = 40
-y = 30
 
-request = "https: // tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid = {973b4a4b9535f80544f8201767f4c9b1}"
-
-try:
-    response = requests.get(request)
-    if response.status_code == 200:
-        json_response = response.json()
-except requests.exceptions.RequestException as e:
-    print("Request could not be completed.")
