de330b5be76d0ed8589e1c500991fc98268ba4b5
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-12-01 17:15:37 +0200

    patch testing

--- Python/FlaskAPIs/api_patch_top_ten_frank.py
+++ Python/FlaskAPIs/api_patch_top_ten_frank.py
@@ -14,19 +14,25 @@ connection = mysql.connector.connect(
 )
 
 
-@app.route('/post_new_score/')
-def post_new_score():
+@app.route('/patch_new_score/')
+def patch_new_score():
     try:
         args = request.args
         username = str(args.get("username"))
-        passwordhash = str(args.get("passwordhash"))
-        sql = f"INSERT INTO users (username, passwordhash, score) VALUES (\"{username}\",\"{passwordhash}\", 0);;"
+        new_score = str(args.get("new_score"))
+        sql1 = f"SELECT score FROM users WHERE username = \"{username}\";"
+        sql2 = f"UPDATE users SET score = \"{new_score}\" WHERE username = \"{username}\";"
         cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        cursor.close()
-        connection.commit()
-        return result
+        cursor.execute(sql1)
+        result_score = cursor.fetchall()
+        if int(new_score) > int(result_score[0][0]):
+            cursor.execute(sql2)
+            connection.commit()
+            cursor.close()
+        else:
+            print("you did not beat your previous highest score")
+            cursor.close()
+        return f"you beat your previous high score"
 
     except ValueError:
         response = {
