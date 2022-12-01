16f77927176b6c33131230a4d86dc10ce435cbc4
kiianaaa <kiana.aghajani@metropolia.fi>
2022-12-01 17:26:00 +0200

    css

--- React/src/Credits/Credits.js
+++ React/src/Credits/Credits.js
@@ -4,15 +4,18 @@ import React from 'react'
 const Credits = () => {
     return (
         <>
-        <ul>
-            <li>Khai Cao</li>
-            <li>Kiana Aghajani</li>
-            <li>Francesco Natanni</li>
-            <li>Jenni Hallikas</li>
-            <li>Eiaki V. Morooka</li>
-</ul>
+            <ul class="cul">
+                <li class="back" id="hb">Backend people:</li>
+                <li class="back">Francesco Natanni</li>
+                <li class="back">Eiaki V. Morooka</li>
+                <li class="data" id="hd">Database people:</li>
+                <li class="data">Jenni Hallikas</li>
+                <li class="front" id="hf">Frontend people:</li>
+                <li class="front">Khai Cao</li>
+                <li class="front">Kiana Aghajani</li>
+            </ul>
         </>
-        
+
     )
 }
 
