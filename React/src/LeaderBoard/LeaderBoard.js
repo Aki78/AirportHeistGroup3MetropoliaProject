362d5bf79729765c27a384437d02d6aaaeb11768
MKRadius <caomanhkhai110@gmail.com>
2022-12-05 05:31:56 +0200

    Fix leaderboard default display

--- React/src/LeaderBoard/LeaderBoard.js
+++ React/src/LeaderBoard/LeaderBoard.js
@@ -2,7 +2,7 @@ import './leadbrd.css'
 import React, { useState, useEffect } from 'react'
 
 const LeaderBoard = () => {
-    const [userInfo, setUserInfo] = useState([]);
+    const [userInfo, setUserInfo] = useState(Array.from(Array(10).keys()));
     const request = "http://127.0.0.1:5000/top_ten";
 
     useEffect(() => {
