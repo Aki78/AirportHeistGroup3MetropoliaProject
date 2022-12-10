180f96b964fe53e44bd2ef8e51b64b821fa263bb
MKRadius <caomanhkhai110@gmail.com>
2022-12-10 23:32:32 +0200

    Add change password form

--- React/src/Account/AccountManagement.js
+++ React/src/Account/AccountManagement.js
@@ -23,9 +23,41 @@ const AccountManagement = ({username, callbackUsername ,  callbackSignedIn}) => 
     return (
         <>
             <h1>Profile</h1>
-            <p>Username: {username}</p>
+            <p>Username: <strong>{username}</strong></p>
 
-            <button onClick={handleSignOutEvent}>Sign out</button>
+            <div className="change-password">
+                <div className="cpw-old-password">
+                    <input
+                        type="password"
+                        placeholder='Old password'
+                        //onChange={e => setAccPassword(e.target.value)}
+                    />
+                </div>
+
+                <div className="cpw-new-password">
+                    <input
+                        type="password"
+                        placeholder='New password'
+                        //onChange={e => setAccPassword(e.target.value)}
+                    />
+                </div>
+
+                <div className="cpw-cfn-password">
+                    <input
+                        type="password"
+                        placeholder='Confirm password'
+                        //onChange={e => setPasswordConfirm(e.target.value)}
+                    />
+                </div>
+
+                <button>Change password</button>
+            </div>
+
+            <div className="line"></div>
+
+            <div className="sign-out">
+                <button onClick={handleSignOutEvent}>Sign out</button>
+            </div>
         </>
 
     )
