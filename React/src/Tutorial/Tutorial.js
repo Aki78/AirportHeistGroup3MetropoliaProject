1f49fbabf62bab1ecb451de823eb0a1ece880a94
kiianaaa <kiana.aghajani@metropolia.fi>
2022-12-11 14:30:59 +0200

    crs

--- React/src/Tutorial/Tutorial.js
+++ React/src/Tutorial/Tutorial.js
@@ -7,45 +7,61 @@ const Tutorial = () => {
             <div className='container'>
                 <main>
                     <h2 id='how'>How To Play Aairport Heist</h2>
+                    <div className='steps'>
+                        <ol>
+                            <li>
+                                Go to our website: <a href='https://aki78.pythonanywhere.com/home'>https://aki78.pythonanywhere.com/home</a>
+                            </li>
+                            <li>
+                                Then click on the game!
+                            </li>
+                            <li>
+                                Wit for it to load...
+                            </li>
+                            <li>
+                                
+                            </li>
+                        </ol>
+                    </div>
                 </main>
-                
-                <section>
-                    <h1>Glossary</h1>
-                    <ol>
-                        <li>
-                            <p>Flight Limit:</p>
-                            <p>You were able to commandeer a small plane at Helsinki Airport.
-                            Due to the size of the plane's fuel tank flights are limited to 800km.
-                                Airports with-in 800km travel range are showenv on a map.
-                            </p>
-                        </li>
-                        <li>
-                            <p>Evading Capture:</p>
-                            <p>
-                                Interpol moves from airport to airport at random. You can see in which city interpol is
-                                and must avoid flying to the same airport. The game is over if interpol lands in the same airport as you.
-                            </p>
-                        </li>
-                        <li>
-                            <p>Ticket:</p>
-                            <p>
-                                You start the game with 5000€. Each flight cost money based on how long the flight is.
-                            </p>
-                        </li>
-                        <li>
-                            <p>Points:</p>
-                            <p>Money is equal to points, so when the player reaches portugal their score is
-                                equal to how much money they have left.</p>
-                        </li>
-                        <li>
-                            <p>Stealing:</p>
-                            <p>
-                                In order to get more money
-                                and points, you can choose to steal. Stealing is done after arriving at an airport.</p>
-                        </li>
-                    </ol>
-                </section>
-
+                <div className='brd'>
+                    <section>
+                        <h1>Glossary</h1>
+                        <ol>
+                            <li>
+                                <p>Flight Limit:</p>
+                                <p>You were able to commandeer a small plane at Helsinki Airport.
+                                    Due to the size of the plane's fuel tank flights are limited to 800km.
+                                    Airports with-in 800km travel range are showenv on a map.
+                                </p>
+                            </li>
+                            <li>
+                                <p>Evading Capture:</p>
+                                <p>
+                                    Interpol moves from airport to airport at random. You can see in which city interpol is
+                                    and must avoid flying to the same airport. The game is over if interpol lands in the same airport as you.
+                                </p>
+                            </li>
+                            <li>
+                                <p>Ticket:</p>
+                                <p>
+                                    You start the game with 5000€. Each flight cost money based on how long the flight is.
+                                </p>
+                            </li>
+                            <li>
+                                <p>Points:</p>
+                                <p>Money is equal to points, so when the player reaches portugal their score is
+                                    equal to how much money they have left.</p>
+                            </li>
+                            <li>
+                                <p>Stealing:</p>
+                                <p>
+                                    In order to get more money
+                                    and points, you can choose to steal. Stealing is done after arriving at an airport.</p>
+                            </li>
+                        </ol>
+                    </section>
+                </div>
             </div>
         </>
 
